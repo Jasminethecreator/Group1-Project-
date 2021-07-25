a79a36472231ba6ef5b11738c768055ae922db74
aannabell <86562168+aannabell@users.noreply.github.com>
2021-07-25 11:26:24 -0400

    Update gpls_script.js

--- gpls_script.js
+++ gpls_script.js
@@ -1,9 +1,9 @@
 function toggleDarkMode(){
   var background =document.getElementById("bg");
-  if (background.style.background=='black'){ // black
-    background.style.background='peachpuff'; // peachpuff
+  if (background.style.background=="black"){ // black
+    background.style.background="peachpuff"; // peachpuff
   }
   else{
-    background.style.background='black'; // black
+    background.style.background="black"; // black
   }
 }
