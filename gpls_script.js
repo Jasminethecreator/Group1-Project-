b45f1fb77a1eaebaff95cea94b36fd44a2e5998b
Jasminethecreator <jasminebienaime@outlook.com>
2022-02-22 08:48:37 -0500

    rename file to app.js

--- gpls_script.js
+++
@@ -1,9 +0,0 @@
-function toggleDarkMode(){
-  var background =document.getElementById("bg");
-  if (background.style.background=="black"){ // black
-    background.style.background="peachpuff"; // peachpuff
-  }
-  else{
-    background.style.background="black"; // black
-  }
-}
